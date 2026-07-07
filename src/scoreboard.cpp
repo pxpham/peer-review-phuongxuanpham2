885ac90943c74d711d8313584b79686a82c37d24
Anthony Oraha <anthonyoraha06@yahoo.com>
2026-07-07 04:41:37 +0000

    All tests passed!

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -7,28 +7,66 @@ int calculateTotal(const int scores[], int size) {
     // TODO:
     // If the array is null or the size is invalid, return 0.
     // Otherwise, return the sum of all scores.
-    return 0;
+    if(size <= 0 || scores == NULL) {
+        return 0;
+    }
+
+    int total = 0;
+
+    for(int i = 0; i < size; i++) {
+        total += scores[i];
+    }
+
+    return total;
 }
 
 double calculateAverage(const int scores[], int size) {
     // TODO:
     // If the array is null or the size is invalid, return 0.0.
     // Otherwise, return the total divided by size.
-    return 0.0;
+    if(size <= 0 || scores == NULL) {
+        return 0.0;
+    }
+
+    return calculateTotal(scores, size) / double(size);
 }
 
 int findLowest(const int scores[], int size) {
     // TODO:
     // If the array is null or the size is invalid, return 0.
     // Otherwise, return the smallest score.
-    return 0;
+    if(size <= 0 || scores == NULL) {
+        return 0;
+    }
+
+    int lowest = scores[0];
+
+    for(int i = 0; i < size; i++) {
+        if(scores[i] < lowest) {
+            lowest = scores[i];
+        }
+    }
+
+    return lowest;
 }
 
 int findHighest(const int scores[], int size) {
     // TODO:
     // If the array is null or the size is invalid, return 0.
     // Otherwise, return the largest score.
-    return 0;
+    if(size <= 0 || scores == NULL) {
+        return 0;
+    }
+
+    int highest = scores[0];
+
+    for(int i = 0; i < size; i++) {
+        if(scores[i] > highest) {
+            highest = scores[i];
+        }
+    }
+
+    return highest;
 }
 
 int findScore(const int scores[], int size, int target) {
@@ -36,6 +74,14 @@ int findScore(const int scores[], int size, int target) {
     // Search the array from left to right.
     // Return the index where target is found.
     // Return -1 when target is not found.
+    if(size <= 0 || scores == NULL) {
+        return -1;
+    }
+
+    for(int i = 0; i < size; i++) {
+        if(scores[i] == target) return i;
+    }
+
     return -1;
 }
 
@@ -43,6 +89,20 @@ void sortScores(int scores[], int size) {
     // TODO:
     // Sort the array from lowest to highest.
     // A selection sort works well for this lab.
+
+    for (int i = 0; i < size - 1; i++) {
+        int lowestIndex = i;
+
+        for (int j = i + 1; j < size; j++) {
+            if (scores[j] < scores[lowestIndex]) {
+                lowestIndex = j;
+            }
+        }
+
+        int temp = scores[i];
+        scores[i] = scores[lowestIndex];
+        scores[lowestIndex] = temp;
+    }
 }
 
 void printScores(const int scores[], int size) {
@@ -64,5 +124,5 @@ void printScores(const int scores[], int size) {
 
 bool isValidSize(int size) {
     // TODO: Return true when size is greater than 0.
-    return false;
+    return (size > 0);
 }
